5d0312d1d50947990cae4f1baac0de25ba6d4837
Vasili2797 <nashvilliv1@gmail.com>
2023-10-02 06:45:30 -0400

    Added files to deploy on Netlify

---
+++ index.js
@@ -0,0 +1,16 @@
+function buttonFunction() {
+    document.write("This is the method that we have all been waiting for:");
+    console.log("I just moved over the thing");
+};
+
+let queryContactInfo = document.getElementById("contactlink");
+let newNode = document.createElement('div');
+newNode.appendChild(contactlink());
+
+function contactlink() {
+    document.createElement('<p>For all the services, please include your name, email address, and reason for contact and I will get back to as');
+}
+
+function description() {
+    console.log("What I want to do here is that I want the thing to show a small description paragraph, then on the bottom for it to have two different buttons, which will be, live and code. At this moment, just code itself is satisfactory because I do not have the program going live for now");
+}
