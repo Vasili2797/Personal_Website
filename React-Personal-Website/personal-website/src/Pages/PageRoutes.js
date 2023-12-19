d7549fcf9bc47c295fea03e11c7abed47ceb4829
Vasili2797 <nashvilliv1@gmail.com>
2023-12-18 23:42:30 -0500

    Changed to BrowserRouter and Switch

--- React-Personal-Website/personal-website/src/Pages/PageRoutes.js
+++ React-Personal-Website/personal-website/src/Pages/PageRoutes.js
@@ -1,20 +1,28 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { UseForm } from "react-hook-form";
 import About from "./About";
 import GitHub from "./GitHub";
 import LinkedIn from "./LinkedIn";
 import MyProjects from "./MyProjects";
 import Resume from "./Resume";
+import Home from "./Home";
+import Navigation from "../Components/Navigation";
 function PageRoutes() {
   return (
     <>
-      <Routes>
-        <Route path="About" element={<About />} />
-        <Route path="GitHub" element={<GitHub />} />
-        <Route path="LinkedIn" element={<LinkedIn />} />
-        <Route path="MyProjects" element={<MyProjects />} />
-        <Route path="Resume" element={<Resume />} />
-      </Routes>
+      <Navigation />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={<Home />}>
+            <Home />
+          </Route>
+          <Route path="About" component={<About />} />
+          <Route path="GitHub" element={<GitHub />} />
+          <Route path="LinkedIn" element={<LinkedIn />} />
+          <Route path="MyProjects" element={<MyProjects />} />
+          <Route path="Resume" element={<Resume />} />
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
