1fe0d696dea371674a6af7fe371e812dbd5958b0
Vasili2797 <nashvilliv1@gmail.com>
2023-12-19 00:03:51 -0500

    Added index page and centerd the title and image

--- React-Personal-Website/personal-website/src/Pages/Home.js
+++ React-Personal-Website/personal-website/src/Pages/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Row, Col } from "react-bootstrap";
+import IndexImage from "../Files/image/index_picture.jpg";
+import Image from "react-bootstrap/Image";
 
 const standardPanel = (contentVariable) => {
   return (
@@ -11,10 +13,27 @@ const standardPanel = (contentVariable) => {
   );
 };
 
+const indexImage = () => {
+  <Image src={{ IndexImage }} fluid alt="Index Image" id="indexPageImage" />;
+};
+
 const Home = () => {
   const componentContent = (
     <>
-      <h1>Personal Website of Vasili Nashvilli</h1>
+      <Row className="justify-content-center mt-5">
+        <Col xs={12} sm={2} md={3}>
+          <img
+            align="center"
+            src={IndexImage}
+            alt="Index Image"
+            id="indexPageImage"
+          />
+        </Col>
+      </Row>
+
+      <Row className="text-center mt-3">
+        <h1>Welcome to The Personal Website of Vasili Nashvilli</h1>
+      </Row>
       <p>
         Welcome to my personal website. This is the place where you can see
         different projects I developed. Thank you for visiting!
