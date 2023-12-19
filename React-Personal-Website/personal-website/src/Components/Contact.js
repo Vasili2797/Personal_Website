4d2c137c5d02fa07e983b3a02f38fff9e39ea547
Vasili2797 <nashvilliv1@gmail.com>
2023-12-19 00:01:49 -0500

    Added and styled contact information form

--- React-Personal-Website/personal-website/src/Components/Contact.js
+++ React-Personal-Website/personal-website/src/Components/Contact.js
@@ -14,61 +14,65 @@ const Contact = () => {
   console.log("Thank You");
   const componentContent = (
     <>
-      <h2>Contact</h2>
-      <p>
-        For all the services, please include your name, email address, and
-        reason for contact and I will get back as soon as possible!
-      </p>
-      <br />
-      <form
-        action="https://formsubmit.co/bjornbjornssonn@gmail.com"
-        method="POST"
-      >
-        <label for="name" id="name">
-          name
-        </label>
-        <input
-          name="name"
-          value=""
-          type="name"
-          id="name"
-          placeholder="name"
-          required
-        />
+      <Row className="justify-content-center mt-5">
+        <Col xs={12} sm={6} md={8}>
+          <h2>Contact</h2>
+          <p>
+            For all the services, please include your name, email address, and
+            reason for contact and I will get back as soon as possible!
+          </p>
+          <br />
+          <form
+            action="https://formsubmit.co/bjornbjornssonn@gmail.com"
+            method="POST"
+          >
+            <label for="name" id="name">
+              name
+            </label>
+            <input
+              name="name"
+              value=""
+              type="name"
+              id="name"
+              placeholder="name"
+              required
+            />
 
-        <div>
-          <label for="email" id="email">
-            email address
-          </label>
-          <input
-            name="email"
-            value=""
-            type="email"
-            id="email"
-            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
-            placeholder="email@..."
-            title="Not Valid"
-            required
-          />
-        </div>
+            <div>
+              <label for="email" id="email">
+                email address
+              </label>
+              <input
+                name="email"
+                value=""
+                type="email"
+                id="email"
+                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
+                placeholder="email@..."
+                title="Not Valid"
+                required
+              />
+            </div>
 
-        <br />
-        <label for="Services" id="Services">
-          services required
-        </label>
-        <br />
-        <textarea
-          name="Service"
-          cols="50"
-          rows="10"
-          placeholder="Services "
-          required
-        ></textarea>
-        <br />
-        <button type="submit" id="submit">
-          Submit
-        </button>
-      </form>
+            <br />
+            <label for="Services" id="Services">
+              services required
+            </label>
+            <br />
+            <textarea
+              name="Service"
+              cols="50"
+              rows="10"
+              placeholder="Services "
+              required
+            ></textarea>
+            <br />
+            <button type="submit" id="submit">
+              Submit
+            </button>
+          </form>
+        </Col>
+      </Row>
     </>
   );
   return standardPanel(componentContent);
